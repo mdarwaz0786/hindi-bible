e607bdac8726e38883ff9daaf1410313d0678d16
Md Arwaz <mdarwaznew2017@gmail.com>
2025-05-07 13:28:17 +0530

    add splash screen

--- App.js
+++ App.js
@@ -1,8 +1,13 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import StackNavigation from './src/Navigation/StactNavigation.js';
+import SplashScreen from 'react-native-splash-screen';
 
 const App = () => {
+  useEffect(() => {
+    SplashScreen.hide();
+  }, []);
+
   return (
     <>
       <NavigationContainer>

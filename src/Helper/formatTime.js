e3c495c1805f095aed9bdbf400d1f59e52265589
Md Arwaz <mdarwaznew2017@gmail.com>
2025-05-07 14:48:21 +0530

    fix bugs

--- src/Helper/formatTime.js
+++ src/Helper/formatTime.js
@@ -1,8 +1,11 @@
+/* eslint-disable no-extra-semi */
 const formatTime = (time) => {
+  if (!time) {
+    return;
+  };
   const minutes = Math.floor(time / 60);
   const seconds = Math.floor(time % 60);
   return `${minutes}:${seconds < 10 ? '0' : ''}${seconds}`;
 };
-
 
 export default formatTime;
